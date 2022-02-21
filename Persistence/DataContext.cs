16509658a3af337d357d6de0f612604d2687fd5b
Mark <serov@angstrem.tech>
2022-02-21 20:18:39 +0300

    init

---
+++ Persistence/DataContext.cs
@@ -0,0 +1,22 @@
+﻿using Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Persistence;
+public class DataContext : DbContext
+{
+    public DataContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<Activity> Activities { get; set; }
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //{
+    //    var configuration = new ConfigurationBuilder()
+    //        .SetBasePath(Directory.GetCurrentDirectory())
+    //        .AddJsonFile("appsettings.json")
+    //        .Build();
+
+    //    var connectionString = configuration.GetConnectionString("AppDb");
+    //    optionsBuilder.UseSqlite(connectionString);
+    //}
+}
