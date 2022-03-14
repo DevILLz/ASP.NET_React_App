d4aa6cb240969ddfb42fc816b2b469e75e51ee38
Mark Serov <serov@angstrem.tech>
2022-03-14 16:48:56 +0300

    Complete CRUD

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -1,25 +1,34 @@
 ﻿using Application.Activities;
 using Domain;
-using MediatR;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
 public class ActivitiesController : BaseAPIController
 {
-    private readonly IMediator mediator;
-
-    public ActivitiesController(IMediator mediator)
-    {
-        this.mediator = mediator;
-    }
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
-        return await mediator.Send(new List.Query());
+        return await Mediator.Send(new List.Query());
     }
     [HttpGet("{id}")]
     public async Task<ActionResult<Activity>> GetActivity(Guid id)
     {
-        return Ok();
+        return await Mediator.Send(new Details.Query{Id = id});
+    }
+    [HttpPost]
+    public async Task<IActionResult> CreateActivity(Activity activity)
+    {
+        return Ok (await Mediator.Send(new Create.Command {Activity = activity}));
+    }
+    [HttpPut("{id}")]
+    public async Task<IActionResult> EditActivity(Guid id, Activity activity)
+    {
+        activity.Id = id;
+        return Ok (await Mediator.Send(new Edit.Command {Activity = activity}));
+    }
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> DeleteActivity(Guid id)
+    {
+        return Ok (await Mediator.Send(new Delete.Command {Id = id}));
     }
 }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,8 +1,4 @@
-using Application.Activities;
-using MediatR;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.OpenApi.Models;
-using Persistence;
+using API.Extensions;
 
 namespace API
 {
@@ -17,25 +13,7 @@ namespace API
 
 
         // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "WebAPIv6", Version = "v1" });
-            });
-            services.AddDbContext<DataContext>(opt =>
-            {
-                opt.UseSqlite(configuration.GetConnectionString("DefaultConnection"));
-            });
-            services.AddCors(opt => {
-                opt.AddPolicy("CorsPolicy", policy => {
-                    policy.AllowAnyMethod().AllowAnyHeader().WithOrigins("http://localhost:3000");
-                });
-            });
-            services.AddMediatR(typeof(List.Handler).Assembly);
-        }
+        public void ConfigureServices(IServiceCollection services) => services.AddApplicationServices(configuration);
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
