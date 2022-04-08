2094a798ae73135647e43dcd3bd77d78f1035fc9
Mark Serov <serov@angstrem.tech>
2022-04-08 16:24:13 +0300

    fix

--- client-app/src/features/profiles/ProfileFollowings.tsx
+++ client-app/src/features/profiles/ProfileFollowings.tsx
@@ -1,9 +1,6 @@
-import PhotoUploadWidget from "app/common/imageUpload/PhotoUploadWidget";
-import { Photo, Profile } from "app/models/profile";
 import { useStore } from "app/stores/store";
 import { observer } from "mobx-react-lite";
-import { SyntheticEvent, useEffect, useState } from "react";
-import { Button, Card, Grid, Header, Image, Tab } from "semantic-ui-react";
+import { Card, Grid, Header, Tab } from "semantic-ui-react";
 import ProfileCard from "./ProfileCard";
 export default observer(function ProfileFollowings() {
     const {profileStore: {profile, followings, loadingFollowings, activeTab}} = useStore();
