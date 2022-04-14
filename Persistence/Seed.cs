57bd7fc5aea5c04ac50018ac7cbb791dcde76e21
Mark Serov <serov@angstrem.tech>
2022-04-14 08:57:57 +0300

    Working postgresql

--- Persistence/Seed.cs
+++ Persistence/Seed.cs
@@ -53,7 +53,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Past Activity 1",
-                        Date = DateTime.Now.AddMonths(-2),
+                        Date = DateTime.UtcNow.AddMonths(-2),
                         Description = "Activity 2 months ago",
                         Category = "drinks",
                         City = "London",
@@ -70,7 +70,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Past Activity 2",
-                        Date = DateTime.Now.AddMonths(-1),
+                        Date = DateTime.UtcNow.AddMonths(-1),
                         Description = "Activity 1 month ago",
                         Category = "culture",
                         City = "Paris",
@@ -92,7 +92,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 1",
-                        Date = DateTime.Now.AddMonths(1),
+                        Date = DateTime.UtcNow.AddMonths(1),
                         Description = "Activity 1 month in future",
                         Category = "music",
                         City = "London",
@@ -114,7 +114,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 2",
-                        Date = DateTime.Now.AddMonths(2),
+                        Date = DateTime.UtcNow.AddMonths(2),
                         Description = "Activity 2 months in future",
                         Category = "food",
                         City = "London",
@@ -136,7 +136,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 3",
-                        Date = DateTime.Now.AddMonths(3),
+                        Date = DateTime.UtcNow.AddMonths(3),
                         Description = "Activity 3 months in future",
                         Category = "drinks",
                         City = "London",
@@ -158,7 +158,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 4",
-                        Date = DateTime.Now.AddMonths(4),
+                        Date = DateTime.UtcNow.AddMonths(4),
                         Description = "Activity 4 months in future",
                         Category = "culture",
                         City = "London",
@@ -175,7 +175,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 5",
-                        Date = DateTime.Now.AddMonths(5),
+                        Date = DateTime.UtcNow.AddMonths(5),
                         Description = "Activity 5 months in future",
                         Category = "drinks",
                         City = "London",
@@ -197,7 +197,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 6",
-                        Date = DateTime.Now.AddMonths(6),
+                        Date = DateTime.UtcNow.AddMonths(6),
                         Description = "Activity 6 months in future",
                         Category = "music",
                         City = "London",
@@ -219,7 +219,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 7",
-                        Date = DateTime.Now.AddMonths(7),
+                        Date = DateTime.UtcNow.AddMonths(7),
                         Description = "Activity 7 months in future",
                         Category = "travel",
                         City = "Berlin",
@@ -241,7 +241,7 @@ namespace Persistence
                     new Activity
                     {
                         Title = "Future Activity 8",
-                        Date = DateTime.Now.AddMonths(8),
+                        Date = DateTime.UtcNow.AddMonths(8),
                         Description = "Activity 8 months in future",
                         Category = "drinks",
                         City = "London",
